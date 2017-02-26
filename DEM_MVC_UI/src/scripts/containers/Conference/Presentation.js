223adfb7f2b850332ef46f6bdf49eead5b98e650
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-02-26 22:02:16 +0200

    deleted unnecessary subclass

--- DEM_MVC_UI/src/scripts/containers/Conference/Presentation.js
+++ DEM_MVC_UI/src/scripts/containers/Conference/Presentation.js
@@ -6,14 +6,16 @@ import Hidden from 'components/Hidden';
 
 const Presentation = ({children}) => (
     <div>
-        <Row className={'Row-test'}>
-            <Hidden className={'Hidden-test'} md={'exact'}>{'default test'}</Hidden>
-            <Container className={'Container-test'}>{'default test'}</Container>
-        </Row>
-        {children}
-        <Row className={'Row-test'}>
-            <Column className={'Column-test'} xs={1} lg={6} lgOffset={6}>{'ok'}</Column>
-        </Row>
+        <Container className={'Container-test'}>
+            <Row className={'Row-test'}>
+                <Hidden className={'Hidden-test'} md={'exact'}>{'default test'}</Hidden>
+                {'default test'}
+            </Row>
+            {children}
+            <Row className={'Row-test'}>
+                <Column className={'Column-test'} xs={1} lg={6} lgOffset={6}>{'ok'}</Column>
+            </Row>
+        </Container>
     </div>
 );
 Presentation.propTypes = {
